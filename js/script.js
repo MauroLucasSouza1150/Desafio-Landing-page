d53799be1774c7f952767db38b7fe2041cc57fe0
Mauro Lucas <maurolucas2014@gmail.com>
2024-02-26 20:59:41 -0300

    criando a janela modal

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log("Testadando 1,2,3 ...");
+document.querySelector(".btn__send").addEventListener("click", (e) => {
+    e.preventDefault()
+    document.querySelector(".modal__send").showModal()
+});
+
+document.querySelector(".btn__closed").addEventListener("click", () => {
+    document.querySelector(".modal__send").close()
+});
